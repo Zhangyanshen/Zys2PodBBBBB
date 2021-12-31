db61f7e454724439c86491a466fd1237dfac656b
Jensen <zys2@meitu.com>
2021-12-31 11:18:26 +0800

    首次提交

---
+++ Zys2PodBBBBB/Classes/YSTool.h
@@ -0,0 +1,16 @@
+//
+//  YSTool.h
+//  Pods-Zys2PodBBBBB_Example
+//
+//  Created by zhangyanshen on 2021/12/31.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YSTool : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Zys2PodBBBBB/Classes/YSTool.m
@@ -0,0 +1,12 @@
+//
+//  YSTool.m
+//  Pods-Zys2PodBBBBB_Example
+//
+//  Created by zhangyanshen on 2021/12/31.
+//
+
+#import "YSTool.h"
+
+@implementation YSTool
+
+@end
